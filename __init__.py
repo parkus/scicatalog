0976819ed026c6db4ef9a45c2529bad14a912bab
parke <parke.loyd@gmail.com>
2019-10-09 17:56:38 -0700

    python 3 compatability added

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
+from __future__ import division, print_function, absolute_import
 __author__ = 'Parke Loyd'
 
-from scicatalog import SciCatalog
-import export
+from .scicatalog import SciCatalog
+from . import export
